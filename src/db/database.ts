e84ba95fb4dceec20eaaa0d20660a9b9a370398a
hadi <hadihe2002@gmail.com>
2023-09-15 13:15:05 +0330

    FIX

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,7 +1,7 @@
 import { connect } from "mongoose";
 
 const mongoDBUrl = process.env.MONGO_DB_URL || "mongodb://0.0.0.0:27017/";
-const mongoDBName = process.env.MONGO_DB_NAME || "task-manager-db";
+const mongoDBName = process.env.MONGO_DB_NAME || "task-manager-api";
 
 export async function dbConfig() {
   await connect(mongoDBUrl, {
