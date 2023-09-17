488f561dac84b932335e7f964fee3a8448106253
hadi <hadihe2002@gmail.com>
2023-09-17 11:19:02 +0330

    FIX: add fonts and change template engine

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import express, { NextFunction } from "express";
 import dotenv from "dotenv";
-import mustacheExpress from "mustache-express";
 import { dbConfig } from "./db/database";
 import { taskRouter } from "./modules/task/task.router";
 import { userRouter } from "./modules/user/user.router";
+
+import consolidate from "consolidate";
 import path from "path";
 dotenv.config();
 dbConfig().catch((err) => console.log(err));
@@ -12,13 +13,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(taskRouter);
 app.use(userRouter);
 
-app.set("views", `${__dirname}/public`);
-app.set("view engine", "mustache");
-app.engine("mustache", mustacheExpress());
+app.set("view engine", "html");
+app.engine("html", consolidate.swig);
+
+app.set("views", path.join(__dirname, "public"));
 app.use(express.static(path.join(__dirname, "public")));
 
 const mainRouter = express.Router();
